31da5c95112ca031b1ab31eecd1bb49b73f317a7
Ruben <rubensere@gmail.com>
2020-03-20 14:31:24 +0100

    finding and showing article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,16 @@ class ArticlesController < ApplicationController
 
     def new
         @article = Article.new
-        @article.title = 'Demo'
     end
 
     def create
         @article = Article.create(title: params[:article][:title], content: params[:article][:content])
 
         render json: @article
+    end
+
+    def show
+        @article = Article.find(params[:id])
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "bienvenida", to: "home#index"
   get "articles/new", to: "articles#new"
-  
+  get "articles/:id", to: "articles#show"
+
   post "articles", to: "articles#create"
 end
