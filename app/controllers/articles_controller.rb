0a9cdd0adf46e2ed3756c8f15c52b462f1e1128c
Ruben <rubensere@gmail.com>
2020-03-21 19:36:05 +0100

    deleting articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,5 +24,12 @@ class ArticlesController < ApplicationController
 
         redirect_to @article
     end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to root_path
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
-  get "bienvenida", to: "home#index"
-  get "articles/new", to: "articles#new"
-  get "articles/:id", to: "articles#show"
-  get "articles/:id/edit", to: "articles#edit"
+  root to: "home#index"
 
-  patch "articles/:id", to: "articles#update", as: :article
+  get 'bienvenida', to: 'home#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show'
+  get 'articles/:id/edit', to: 'articles#edit'
 
-  post "articles", to: "articles#create"
+  patch 'articles/:id', to: 'articles#update', as: :article
+
+  post 'articles', to: 'articles#create'
+
+  delete 'articles/:id', to: 'articles#destroy'
 end
